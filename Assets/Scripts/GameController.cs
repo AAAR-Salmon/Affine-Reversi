c7300530e4ca6b63dac754b630a72c554c522f4a
AAAR Salmon <tenzikujohn@gmail.com>
2021-08-06 13:18:29 +0900

    Add: 盤面のループ（上端から下端へのカーソル移動）を実装

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -17,23 +17,26 @@ public class GameController : MonoBehaviour {
 	private GameObject placeableHintPrefab;
 	private DiskColor[,] arrayColor;
 	private GameObject[,] arrayDisk;
-	private List<GameObject> arrayPlaceableHint;
+	private List<GameObject> listPlaceableHint;
 	private const int rowSize = 8;
 	private const int columnSize = 8;
-	[SerializeField] private Color colorBlack = Color.black;
-	[SerializeField] private Color colorWhite = Color.white;
+	[SerializeField] private Color colorDiskBlack = Color.black;
+	[SerializeField] private Color colorCursorBlack = Color.black;
+	[SerializeField] private Color colorDiskWhite = Color.white;
+	[SerializeField] private Color colorCursorWhite = Color.white;
 	[SerializeField] private float offsetX = -3.5f;
 	[SerializeField] private float offsetY = -3.5f;
 	[SerializeField] private float unitX = 1.0f;
 	[SerializeField] private float unitY = 1.0f;
 
 	private void Awake() {
+		turn = DiskColor.Black;
 		diskPrefab = (GameObject) Resources.Load("Prefabs/DiskPrefab");
 		placeableHintPrefab = (GameObject) Resources.Load("Prefabs/PlaceableHintPrefab");
 		cursor = this.transform.Find("Cursor").gameObject;
 		arrayColor = new DiskColor[rowSize, columnSize];
 		arrayDisk = new GameObject[rowSize, columnSize];
-		arrayPlaceableHint = new List<GameObject>();
+		listPlaceableHint = new List<GameObject>();
 		for (int i = 0; i < rowSize; i++) {
 			for (int j = 0; j < columnSize; j++) {
 				arrayColor[i, j] = DiskColor.None;
@@ -48,7 +51,7 @@ public class GameController : MonoBehaviour {
 
 	// Start is called before the first frame update
 	void Start() {
-		turn = DiskColor.Black;
+		cursor.GetComponent<SpriteRenderer>().color = colorCursorBlack;
 		cursor.transform.position = this.transform.position + Vector3FromInt3(cursorPos.x, cursorPos.y, -1);
 		RefreshHint(turn);
 	}
@@ -57,42 +60,50 @@ public class GameController : MonoBehaviour {
 	void Update() {
 		// キー入力による移動
 		if (Input.GetKeyDown(KeyCode.UpArrow)) {
-			cursorPos.y += 1;
+			cursorPos.y = (cursorPos.y + 1) % rowSize;
 			cursor.transform.position = this.transform.position + Vector3FromInt3(cursorPos.x, cursorPos.y, -1);
 		}
 		if (Input.GetKeyDown(KeyCode.DownArrow)) {
-			cursorPos.y -= 1;
+			cursorPos.y = (cursorPos.y + rowSize - 1) % rowSize;
 			cursor.transform.position = this.transform.position + Vector3FromInt3(cursorPos.x, cursorPos.y, -1);
 		}
 		if (Input.GetKeyDown(KeyCode.LeftArrow)) {
-			cursorPos.x -= 1;
+			cursorPos.x = (cursorPos.x + columnSize - 1) % columnSize;
 			cursor.transform.position = this.transform.position + Vector3FromInt3(cursorPos.x, cursorPos.y, -1);
 		}
 		if (Input.GetKeyDown(KeyCode.RightArrow)) {
-			cursorPos.x += 1;
+			cursorPos.x = (cursorPos.x + 1) % columnSize;
 			cursor.transform.position = this.transform.position + Vector3FromInt3(cursorPos.x, cursorPos.y, -1);
 		}
 
 		// 石を置いて色を変更する
 		if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return)) {
-			if (IsPlaceableAt(cursorPos.x, cursorPos.y, turn)) {
+			if (CountTurnoverOnPlace(cursorPos.x, cursorPos.y, turn) > 0) {
 				PlaceDisk(cursorPos.x, cursorPos.y, turn);
 				turn = (turn == DiskColor.Black ? DiskColor.White : DiskColor.Black);
-				cursor.GetComponent<SpriteRenderer>().color = (turn == DiskColor.Black ? colorBlack : colorWhite);
+				cursor.GetComponent<SpriteRenderer>().color = (turn == DiskColor.Black ? colorCursorBlack : colorCursorWhite);
 				RefreshHint(turn);
+				if (listPlaceableHint.Count == 0) {
+					turn = (turn == DiskColor.Black ? DiskColor.White : DiskColor.Black);
+					cursor.GetComponent<SpriteRenderer>().color = (turn == DiskColor.Black ? colorCursorBlack : colorCursorWhite);
+					RefreshHint(turn);
+					if (listPlaceableHint.Count == 0) {
+						// finish
+					}
+				}
 			}
 		}
 	}
 
 	void RefreshHint(DiskColor _c) {
-		foreach (var obj in arrayPlaceableHint) {
+		foreach (var obj in listPlaceableHint) {
 			Destroy(obj);
 		}
-		arrayPlaceableHint.Clear();
+		listPlaceableHint.Clear();
 		for (int i = 0; i < rowSize; i++) {
 			for (int j = 0; j < columnSize; j++) {
-				if (IsPlaceableAt(i, j, _c)) {
-					arrayPlaceableHint.Add(Instantiate(placeableHintPrefab, this.transform.position + Vector3FromInt3(i, j, 0), Quaternion.identity, this.transform));
+				if (CountTurnoverOnPlace(i, j, _c) > 0) {
+					listPlaceableHint.Add(Instantiate(placeableHintPrefab, this.transform.position + Vector3FromInt3(i, j, 0), Quaternion.identity, this.transform));
 				}
 			}
 		}
@@ -102,38 +113,32 @@ public class GameController : MonoBehaviour {
 		return new Vector3(offsetX + unitX * _x, offsetY + unitY * _y, _z);
 	}
 
-	bool IsPlaceableAt(int _x, int _y, DiskColor _c) {
+	int CountTurnoverOnPlace(int _x, int _y, DiskColor _c) {
 		if (!InBoardArea(_x, _y)) {
-			return false;
+			return 0;
 		}
 		if (arrayColor[_x, _y] != DiskColor.None) {
-			return false;
+			return 0;
 		}
 		if (_c == DiskColor.None) {
-			return false;
+			return 0;
 		}
+		int _result = 0;
 		(int, int)[] dpos = new (int, int)[8] { (-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1) };
 		foreach (var (dx, dy) in dpos) {
 			int x = _x + dx;
 			int y = _y + dy;
 			int distance = 1;
-			while (InBoardArea(x, y)) {
-				if (arrayColor[x, y] == DiskColor.None) {
-					break;
-				}
-				if (arrayColor[x, y] == _c) {
-					if (distance == 1) {
-						break;
-					} else {
-						return true;
-					}
-				}
+			while (InBoardArea(x, y) && arrayColor[x, y] != DiskColor.None && arrayColor[x, y] != _c) {
 				x += dx;
 				y += dy;
 				distance++;
 			}
+			if (InBoardArea(x, y) && arrayColor[x, y] == _c) {
+				_result += distance - 1;
+			}
 		}
-		return false;
+		return _result;
 	}
 
 	void PlaceDisk(int _x, int _y, DiskColor _c) {
@@ -145,10 +150,10 @@ public class GameController : MonoBehaviour {
 		case DiskColor.None:
 			break;
 		case DiskColor.Black:
-			color = colorBlack;
+			color = colorDiskBlack;
 			break;
 		case DiskColor.White:
-			color = colorWhite;
+			color = colorDiskWhite;
 			break;
 		default:
 			break;
