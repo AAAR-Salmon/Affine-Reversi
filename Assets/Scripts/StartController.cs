3935e3f174e95d3dfc636a8d6736f7468f51a320
AAAR-Salmon <tenzikujohn@gmail.com>
2021-08-10 21:57:21 +0900

    Add: タイトル画面を追加

---
+++ Assets/Scripts/StartController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartController : MonoBehaviour {
+	[SerializeField] private GameObject fracXSlider;
+	[SerializeField] private GameObject fracYSlider;
+	public void OnStartButtonClick() {
+		ConfigurationSingleton.instance.fracX = Mathf.FloorToInt(fracXSlider.GetComponent<Slider>().value);
+		ConfigurationSingleton.instance.fracY = Mathf.FloorToInt(fracYSlider.GetComponent<Slider>().value);
+		SceneManager.LoadSceneAsync("MainScene", LoadSceneMode.Single);
+	}
+}
