6fd49ed57a73716bf34b788f99c930a8326a43c0
AAAR-Salmon <tenzikujohn@gmail.com>
2021-08-10 21:56:46 +0900

    Refactor: 盤面サイズをSingletonに分ける

---
+++ Assets/Scripts/ConfigurationSingleton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfigurationSingleton {
+	private static ConfigurationSingleton _instance;
+
+	public int fracX;
+	public int fracY;
+
+	public static ConfigurationSingleton instance {
+		get {
+			if (_instance == null) {
+				_instance = new ConfigurationSingleton();
+			}
+			return _instance;
+		}
+	}
+
+	private ConfigurationSingleton() {
+		fracX = 8;
+		fracY = 8;
+	}
+}
