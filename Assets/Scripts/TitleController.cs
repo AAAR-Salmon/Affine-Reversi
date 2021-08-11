981b2b8f57c696373839a2e418d65a196cb6a0db
AAAR-Salmon <tenzikujohn@gmail.com>
2021-08-11 21:09:53 +0900

    Add: Sliderのインジケータのリアルタイム表示を実装

--- Assets/Scripts/TitleController.cs
+++ Assets/Scripts/TitleController.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
-public class StartController : MonoBehaviour {
+public class TitleController : MonoBehaviour {
 	[SerializeField] private GameObject fracXSlider;
 	[SerializeField] private GameObject fracYSlider;
 	public void OnStartButtonClick() {
 		ConfigurationSingleton.instance.fracX = Mathf.FloorToInt(fracXSlider.GetComponent<Slider>().value);
 		ConfigurationSingleton.instance.fracY = Mathf.FloorToInt(fracYSlider.GetComponent<Slider>().value);
 		SceneManager.LoadSceneAsync("MainScene", LoadSceneMode.Single);
+	}
+
+	public void OnChangeFracXSlider() {
+		fracXSlider.transform.Find("Value Indicator").GetComponent<TextMeshProUGUI>().text = Mathf.FloorToInt(fracXSlider.GetComponent<Slider>().value).ToString();
+	}
+
+	public void OnChangeFracYSlider() {
+		fracYSlider.transform.Find("Value Indicator").GetComponent<TextMeshProUGUI>().text = Mathf.FloorToInt(fracYSlider.GetComponent<Slider>().value).ToString();
 	}
 }
