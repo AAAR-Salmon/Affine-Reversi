81b6fec818e9be524ce9aadd089d99dce900bc49
AAAR Salmon <tenzikujohn@gmail.com>
2021-08-13 01:41:42 +0900

    Refactor: SingletonのfracX,fracYメンバがスライダーの変更に応じてすぐに変化するように

--- Assets/Scripts/TitleController.cs
+++ Assets/Scripts/TitleController.cs
@@ -8,17 +8,18 @@ using TMPro;
 public class TitleController : MonoBehaviour {
 	[SerializeField] private GameObject fracXSlider;
 	[SerializeField] private GameObject fracYSlider;
+
 	public void OnStartButtonClick() {
-		ConfigurationSingleton.instance.fracX = Mathf.FloorToInt(fracXSlider.GetComponent<Slider>().value);
-		ConfigurationSingleton.instance.fracY = Mathf.FloorToInt(fracYSlider.GetComponent<Slider>().value);
 		SceneManager.LoadSceneAsync("MainScene", LoadSceneMode.Single);
 	}
 
 	public void OnChangeFracXSlider() {
+		ConfigurationSingleton.instance.fracX = Mathf.FloorToInt(fracXSlider.GetComponent<Slider>().value);
 		fracXSlider.transform.Find("Value Indicator").GetComponent<TextMeshProUGUI>().text = Mathf.FloorToInt(fracXSlider.GetComponent<Slider>().value).ToString();
 	}
 
 	public void OnChangeFracYSlider() {
+		ConfigurationSingleton.instance.fracY = Mathf.FloorToInt(fracYSlider.GetComponent<Slider>().value);
 		fracYSlider.transform.Find("Value Indicator").GetComponent<TextMeshProUGUI>().text = Mathf.FloorToInt(fracYSlider.GetComponent<Slider>().value).ToString();
 	}
 }
